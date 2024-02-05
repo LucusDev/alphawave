5b498bf3e03975173ba30e2d7e1b71881fac0826
LucusDev <htetaungshine20028@gmail.com>
2024-02-05 10:53:29 +0700

    finished api setup for chat

--- chat/views.py
+++ chat/views.py
@@ -1,28 +1,37 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpRequest
 import google.generativeai as genai
 from django.conf import settings
-
+import json
 # Create your views here.
 
 
 genai.configure(api_key=settings.API_KEY)
 
 
-def index(request):
-    request
-    model = genai.GenerativeModel('gemini-pro')
-    chat = model.start_chat(history=[
-        {
-            'role': 'user',
-            'parts': ['i am hungry'],
-        },
-        {
-            'role': 'model',
-            'parts': ['so what?'],
-        }
-    ])
-    response = chat.send_message("suggest me a receipe?")
-    return JsonResponse({
-        "success": True,
-        "message": response.text,
-    })
+def index(request: HttpRequest):
+    try:
+        if request.method == 'POST':
+            req_body = json.loads(request.body)
+            if (req_body['message'] != None):
+                model = genai.GenerativeModel('gemini-pro')
+                chat = model.start_chat(history=[
+                    {
+                        'role': 'user',
+                        'parts': ['i am hungry'],
+                    },
+                    {
+                        'role': 'model',
+                        'parts': ['so what?'],
+                    }
+                ])
+                response = chat.send_message(req_body['message'])
+                return JsonResponse({
+                    "success": True,
+                    "message": response.text,
+                })
+        raise Exception
+    except:
+        return JsonResponse({
+            "success": False,
+            "message": "Server error!",
+        })
