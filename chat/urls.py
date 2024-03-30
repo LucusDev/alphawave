242f512a349302cd65be30008c588b0688382090
LucusDev <htetaungshine20028@gmail.com>
2024-03-30 19:07:09 +0700

    added dynamic recommendations

--- chat/urls.py
+++ chat/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import index
+from .views import index, recommendations
 urlpatterns = [
     path("", index),
+    path("recommendations/", recommendations),
+
 ]
